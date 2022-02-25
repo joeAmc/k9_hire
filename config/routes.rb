8432df8b87e64165696b784a2e7c6c3ebfddecfb
Abbas Naqvi <sma.naqvi@outlook.com>
2022-02-25 11:19:31 +0000

    testing for conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    resources :dogs do
-    resources :bookings, only: [:new, :edit, :update, :create, :show]
+    resources :bookings, only: [:new, :create]
     end
-      resources :bookings, only: [:destroy]
+      resources :bookings, only: [:index, :show, :edit, :update, :destroy]
    end
