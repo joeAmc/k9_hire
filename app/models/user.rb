5ebba63d0c9b39927b5c19f4176f85dbc879ee49
joe mcilroy <joeamcilroy@gmail.com>
2022-04-19 17:31:01 +0100

    navbar image and card tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
   has_many :bookings
   has_many :dogs
   has_many :orders
 
   #  has_many :reviews, through: :booking, dependent: :destroy
   validates :first_name, :last_name, :email, :password, presence: true
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar
+    else
+      image_tag(ava.jpg)
+    end
+  end
 end
